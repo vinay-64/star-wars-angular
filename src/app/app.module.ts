db0dd19fd30cdd80fb379285711afb273e42b3e9
vinaykumar1237 <56100155+vinaykumar1237@users.noreply.github.com>
2023-06-16 16:42:06 +0530

    Routing has been added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { TabsComponent } from './tabs/tabs.component';
@@ -10,34 +10,34 @@ import { ItemComponent } from './item/item.component';
 import { StarWarsService } from './star-wars.service';
 import { LogService } from './log.service';
 import { CreateCharacterComponent } from './create-character/create-character.component';
-// import { StarWarsService } from './star-wars.service';
-// import { TopBarComponent } from './top-bar/top-bar.component';
-// import { ProductListComponent } from './product-list/product-list.component';
-// import { CartComponent } from './cart/cart.component';
+import { HeaderComponent } from './header/header.component';
+
+type PathMatch = 'full' | 'prefix' | undefined;
+
+const routes = [
+  {
+    path: 'characters',
+    component: TabsComponent,
+    children: [
+      { path: '', redirectTo: 'all', pathMatch: 'full' as PathMatch },
+      { path: ':side', component: ListComponent },
+    ],
+  },
+  { path: 'new-character', component: CreateCharacterComponent },
+  { path: '**', redirectTo: '/characters' },
+];
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    FormsModule,
-    // RouterModule.forRoot([{ path: '', component: ProductListComponent }]),
-  ],
   declarations: [
     AppComponent,
     TabsComponent,
     ListComponent,
     ItemComponent,
     CreateCharacterComponent,
-    // TopBarComponent,
-    // ProductListComponent,
-    // CartComponent,
+    HeaderComponent,
   ],
-  bootstrap: [AppComponent],
+  imports: [BrowserModule, FormsModule, RouterModule.forRoot(routes)],
   providers: [StarWarsService, LogService],
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
-
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at https://angular.io/license
-*/
